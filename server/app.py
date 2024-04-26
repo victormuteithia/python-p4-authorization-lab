d0ae09de4cf5cbf456b54221dd261a35ecd59295
victormuteithia <muteithiavictor@gmail.com>
2024-04-26 13:35:03 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -87,12 +87,18 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        articles = [article.to_dict() for article in Article.query.filter_by(is_member_only=True)]
+        return make_response(jsonify(articles), 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session['user_id']:
+            return {"error": "Unauthorized"}, 401
+        article = Article.query.filter_by(id=id, is_member_only=True).first()
+        return article.to_dict()
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
